9b4aedc0c41fa9a05a85703c37340e54e14dc85a
Ortwin Joniaux <ortwinjoniaux@hotmail.com>
2016-11-14 22:26:09 +0100

    Simplified the SetPower message.

--- src/main/java/org/ojothepojo/lifx/message/device/request/SetPower.java
+++ src/main/java/org/ojothepojo/lifx/message/device/request/SetPower.java
@@ -8,7 +8,7 @@ import java.nio.ByteOrder;
 
 public class SetPower extends Message{
 
-    private boolean level;
+    private short level;
 
     public SetPower(boolean level) {
         super((short) 38, (short)21, "192.168.1.255", "00:00:00:00:00:00");
@@ -24,23 +24,18 @@ public class SetPower extends Message{
         super(bytes);
     }
 
-    public void setLevel(boolean level) {
-        this.level = level;
-    }
-
-    public boolean getLevel() {
-        return this.level;
+    private void setLevel(boolean level) {
+        if (level) {
+            this.level = (short)65535;
+        } else {
+            this.level = (short)0;
+        }
     }
 
     @Override
     protected ByteBuffer payloadToBytes() {
         ByteBuffer buf = ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN);
-
-        if (level) {
-            buf.putShort((short)65535);
-        } else {
-            buf.putShort((short)0);
-        }
+        buf.putShort(level);
         return buf;
     }
 
@@ -48,14 +43,7 @@ public class SetPower extends Message{
     public void parsePayload(ByteBuffer buf) {
         buf.rewind();
         buf = buf.order(ByteOrder.LITTLE_ENDIAN);
-        int i = (buf.getShort() & 0xffff);
-        if (i == 65535) {
-            level = true;
-        } else if (i == 0) {
-            level = false;
-        } else {
-            throw new IllegalArgumentException("Power level must be 0 or 65535");
-        }
+        level = (short)(buf.getShort() & 0xffff);
     }
 
     @Override
--- src/test/java/org/ojothepojo/lifx/ClientTest.java
+++ src/test/java/org/ojothepojo/lifx/ClientTest.java
@@ -52,5 +52,4 @@ public class ClientTest {
             throw new RuntimeException(e);
         }
     }
-
 }
